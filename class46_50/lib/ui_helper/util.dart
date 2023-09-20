80bdb943145ffc6c2aebe4c06d6aafa929f5af41
Shahidul islam <Shahidul239.si@gmail.com>
2023-09-20 16:53:45 +0600

    class 51 55

---
+++ class46_50/lib/ui_helper/util.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+TextStyle mTextstyle15({
+  Color textColor = Colors.black,
+  FontWeight fontWeight = FontWeight.normal
+}){
+  return TextStyle(
+    fontSize: 15,
+    color: textColor,
+    fontWeight: fontWeight
+  );
+}
+
+TextStyle mTextstyle21({Color textColor = Colors.black,FontWeight fontWeight = FontWeight.normal}){
+  return TextStyle(
+    fontSize: 21,
+    color: textColor,
+    fontWeight: fontWeight
+  );
+}
+
---
+++ class51_55/android/app/src/main/kotlin/com/example/class51_55/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.class51_55
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
